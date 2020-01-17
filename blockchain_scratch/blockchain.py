add29bd0ec5667edba29d10572db3cb6b4412431
Amir Yunas <ayunas@gmail.com>
2020-01-16 22:01:24 -0600

    refactored blockchain to use single class, and set up Flask backend.

--- blockchain_scratch/blockchain.py
+++ blockchain_scratch/blockchain.py
@@ -1,103 +1,62 @@
 from hashlib import sha256
-import json, random
+import json
+import random
 from time import time
+from uuid import uuid4
+from textwrap import dedent
 
+from flask import Flask,jsonify,request
 
-class Block:
-    def __init__(self,transactions, prev_hash, i, proof=0):
-        self.index = i
-        self.transactions = transactions
-        self.timestamp = time()
-        self.hash = None
-        self.proof = proof
-        self.nonce = 0
-        self.prev_hash = prev_hash
-    
-    def hash_block(self):
-        # self.prev_hash = prev_hash
-        block = {'index': self.index, 'transactions': self.transactions, 'timestamp':self.timestamp, 'hash':self.hash, 'proof':self.proof, 'nonce':self.nonce, 'prev_hash': self.prev_hash }
-        jsoned = json.dumps(block,sort_keys=True)
-        hashed = sha256(jsoned.encode('utf-8')).hexdigest()
-        self.hash = hashed
-        return hashed
-
-    def __repr__(self):
-        block = {'index': self.index, 'transactions': self.transactions, 'timestamp':self.timestamp, 'hash':self.hash, 'proof':self.proof, 'nonce':self.nonce, 'prev_hash': self.prev_hash, }
-        stringified_block = json.dumps(block, sort_keys=True)
-        
-        return stringified_block
-
-class Transaction:
-    def __init__(self,sender,receiver,amount):
-        self.sender = sender
-        self.receiver = receiver
-        self.amount = amount
-    
-    def hash_transaction(self):
-        transaction = {
-            'sender': self.sender,
-            'recipient': self.receiver,
-            'amount': self.amount
-        }
-        jsoned = json.dumps(transaction,sort_keys=True)
-        return sha256(jsoned.encode('utf-8')).hexdigest()
-    
-    def __repr__(self):
-        transaction = {
-            'sender': self.sender,
-            'recipient': self.receiver,
-            'amount': self.amount
-        }
-        return str(transaction)
-
+# import names
 # t = Transaction('amir','nadia',50)
 # print(t)
-
-
 
 class BlockChain:
     def __init__(self):
         self.chain = []
         self.transactions = []
     
-    def add_block(self,exchanges,existing_block=None):
+    # def add_block(self,exchanges,existing_block=None):
 
-        xs = [] #xs stands for exchanges.  the array storing the stringified Transaction objects
-        for x in exchanges:
-            sender,receiver,amount = x
-            t = Transaction(sender,receiver,amount)
-            xs.append(str(t))
+    def new_transaction(self,sender,receiver,amount):
+        # t = Transaction(sender,receiver,amount)
+        # self.transactions.append(str(t))
+        transaction = {
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount
+        }
 
-        self.transactions += xs
+        self.transactions.append(transaction)
+        if len(self.chain):
+            return self.last_block['index'] + 1 #return the index of the next block to add this particular transaction to after the new block is mined.
+        else: return 2
 
-        if existing_block is None:
-            if not len(self.chain):
-                prev_hash,index,proof = 0,1,0
-                genesis = Block(xs,prev_hash,index,proof)
-                genesis.hash_block()
-                self.chain.append(genesis)
-            else: #there exists blocks on the chain
-                prev_hash = self.chain[-1].hash
-                print('prev_hash', prev_hash)
-                last_proof = self.chain[-1].proof
-                print('last_proof', last_proof)
-                index = self.chain[-1].index + 1
-                proof = self.proof_of_work(last_proof,5)
-                print('proof: ',proof)
-                new_block = Block(xs,prev_hash,index,proof)
-                new_block.hash_block()
-                self.chain.append(new_block)
-        else: #block has been passed in
-            if not len(self.chain): #genesis block
-                genesis = block
-                genesis.prev_hash = 0
-                genesis.hash_block()
-                self.chain.append(genesis)
-            else: #there are blocks in the block chain
-                block.prev_hash = self.chain[-1].hash
-                block.index = self.chain[-1].index + 1
-                block.hash_block()
-                self.chain.append(block)
+    def new_block(self, last_proof=0, prev_hash=0):
+
+        if not len(self.chain):
+            last_proof = 0
+        else:
+            last_proof = self.chain[-1]['proof']
+    
+        proof = self.proof_of_work(last_proof,4) #whoever gets the proof of work, then their block will be created with all the transactions pending in the blockchain. then the transactions will be cleared out again
+
+        print('proof', proof)
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions,
+            'proof': proof,
+            'prev_hash': prev_hash if not len(self.chain) else self.hash(self.chain[-1])
+            # 'prev_hash': prev_hash or self.hash(self.chain[-1])
+        }
+
+        self.transactions = []
+        # self.new_transaction('0','miner',1)
+        # if len(block['transactions']):
+        self.chain.append(block)
+        return block
 
     def proof_of_work(self, last_proof,difficulty=3):
         ''' - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
@@ -110,11 +69,11 @@ class BlockChain:
             zeros += '0'
         print('zeros', zeros)
 
-        while self.hash_try(proof,last_proof)[:difficulty] != zeros:
+        while self.hash_proofs(proof,last_proof)[:difficulty] != zeros:
             # print(f'attempt # {proof}', self.hash_try(proof,last_proof))
             proof += 1
         
-        print('found solution:', self.hash_try(proof,last_proof))
+        print('found solution:', self.hash_proofs(proof,last_proof))
         return proof
 
         # x = len(self.transactions)
@@ -128,66 +87,271 @@ class BlockChain:
         # return hashed
          # while sha256(str(proof + last_proof).encode('utf-8')).hexdigest()[:3] != '000':
         #      proof += 1
-       
 
-    def hash_try(self,proof,last_proof):
+    def hash_proofs(self,proof,last_proof):
         new_proof = str(proof) + str(last_proof)
         return sha256(new_proof.encode('utf-8')).hexdigest()
-
-    def hash_blocks(self):
-        for i,b in enumerate(self.chain):
-            if i == 0:
-                prev_hash = 0
-                genesis = b
-                genesis.hash_block(prev_hash)
-            else:
-                prev_hash = self.chain[i-1].hash
-                b.hash_block(prev_hash)
-
-    def set_difficulty(self,difficulty):
-        zeros = ''
-        for i in range(difficulty):
-            zeros += '0'
-        # zeros = zeros[1:]
-        for i,b in enumerate(self.chain):
-            b.hash = zeros + b.hash
-            if i == 0:
-                b.prev_hash = 0
-                continue
-            b.prev_hash = self.chain[i-1].hash
-        return self.chain
-        
-        return zeros
     
-    def check_difficulty(self,difficulty):
-        genesis_hash = self.chain[0].hash
-        for i,c in enumerate(genesis_hash):
-            if c != '0':
-                break
-        return i >= difficulty
-
+    def hash_all(self):
+        hashes = []
+        for b in self.chain:
+            hb = self.hash(b)
+            hashes.append(hb)
+        return hashes
     
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True)
+        hashed_block = sha256(block_string.encode('utf-8')).hexdigest()
+        return hashed_block
+    
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     def blockchain(self):
         for b in self.chain:
             print(b)
             print('\n')
+        return self.chain
         
     def __repr__(self):
         return str(self.chain)
 
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
 
 bc = BlockChain()
-
-an2 = ('amir','nancy',20)
-jj10 = ('joe','jacob',10)
-zz5 = ('zieger','zelda',5)
-ma50 = ('muhammad','abdullah',50)
-jl12 = ('jose','leanna',12)
-
-bc.add_block([an2])
-bc.add_block([jj10,zz5,ma50])
-
+bc.new_block()
+bc.new_block()
+bc.new_block()
+bc.new_transaction('amir','nancy',20)
+bc.new_block()
 bc.blockchain()
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    print(values)
+
+    required = ['sender', 'receiver', 'amount']
+    if not all(t in values for t in required):
+        return 'Missing Values', 400
+
+    index = bc.new_transaction(values['sender'],values['receiver'],values['amount'])
+    response = {'message': f"Transaction will be added to Block {index}"}
+    return jsonify(response)
+
+    # return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': bc.chain,
+        'length': len(bc.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
+
+
+
+
+
+
+
+
+# bc.new_transaction('amir','nancy',20)
+# genesis = bc.new_block()
+# b1 = bc.new_block()
+# b2 = bc.new_block()
+# b3 = bc.new_block()
+# b4 = bc.new_block()
+# b5 = bc.new_block()
+# bc.new_transaction('adam','sally',5)
+# b6 = bc.new_block()
+# b7 = bc.new_block()
+# b8 = bc.new_block()
+
+
+# hashes = bc.hash_all()
+
+# bc.blockchain()
+
+# an2 = ('amir','nancy',20)
+# jj10 = ('joe','jacob',10)
+# zz5 = ('zieger','zelda',5)
+# ma50 = ('muhammad','abdullah',50)
+# jl12 = ('jose','leanna',12)
+
+# first_names = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"]
+
+# class Block:
+#     def __init__(self,transactions, prev_hash, i, proof=0):
+#         self.index = i
+#         self.transactions = transactions
+#         self.timestamp = time()
+#         self.hash = None
+#         self.proof = proof
+#         self.prev_hash = prev_hash
+    
+#     def hash_block(self):
+#         # self.prev_hash = prev_hash
+#         block = {'index': self.index, 'transactions': self.transactions, 'timestamp':self.timestamp, 'hash':self.hash, 'proof':self.proof,'prev_hash': self.prev_hash}
+#         jsoned = json.dumps(block,sort_keys=True)
+#         hashed = sha256(jsoned.encode('utf-8')).hexdigest()
+#         self.hash = hashed
+#         return hashed
+
+#     def __repr__(self):
+#         block = {'index': self.index, 'transactions': self.transactions, 'timestamp':self.timestamp, 'hash':self.hash, 'proof':self.proof,'prev_hash': self.prev_hash}
+#         stringified_block = json.dumps(block, sort_keys=True)
+        
+#         return stringified_block
+
+# class Transaction:
+#     def __init__(self,sender,receiver,amount):
+#         self.sender = sender
+#         self.receiver = receiver
+#         self.amount = amount
+    
+#     def hash_transaction(self):
+#         transaction = {
+#             'sender': self.sender,
+#             'recipient': self.receiver,
+#             'amount': self.amount
+#         }
+#         jsoned = json.dumps(transaction,sort_keys=True)
+#         return sha256(jsoned.encode('utf-8')).hexdigest()
+    
+#     def __repr__(self):
+#         transaction = {
+#             'sender': self.sender,
+#             'recipient': self.receiver,
+#             'amount': self.amount
+#         }
+#         return str(transaction)
+
+
+# def transaction_generator(quantity):
+#     transactions = []
+#     for i in range(quantity):
+#         # n1 = names.get_first_name(gender="male")
+#         # n2 = names.get_first_name(gender="female")
+#         n1 = random.choice(first_names)
+#         n2 = random.choice(first_names)
+#         amount = random.randint(1,100)
+#         transactions.append((n1,n2,amount))
+#     return transactions
+
+# transactions = transaction_generator(15)
+
+# bc.add_block([an2])
+# bc.add_block([jj10,zz5,ma50])
+# for i in range(10):
+#     a = bc.add_block([random.choice(transactions)])
+#     print(type(a),a)
+
+# chain = bc.blockchain()
+# print(len(chain))
+
+
+
+
+    # def add_block(self,exchanges):
+    #     xs = [] #xs stands for exchanges.  the array storing the stringified Transaction objects
+    #     for x in exchanges:
+    #         sender,receiver,amount = x
+    #         t = Transaction(sender,receiver,amount)
+    #         xs.append(str(t))
+
+    #     self.transactions += xs
+
+    #     # if existing_block is None:
+    #     if len(self.chain) == 0:
+    #         prev_hash,index,proof = 0,1,0
+    #         genesis = Block(xs,prev_hash,index,proof)
+    #         genesis.hash_block()
+    #         self.chain.append(genesis)
+    #         return genesis.__dict__
+    #     else: #there exists blocks on the chain
+    #         prev_hash = self.chain[-1].hash
+    #         print('prev_hash', prev_hash)
+    #         last_proof = self.chain[-1].proof
+    #         print('last_proof', last_proof)
+    #         index = self.chain[-1].index + 1
+    #         proof = self.proof_of_work(last_proof,4)
+    #         print('proof: ',proof)
+    #         new_block = Block(xs,prev_hash,index,proof)
+    #         new_block.hash_block()
+    #         self.chain.append(new_block)
+    #         return new_block.__dict__
+
+        # else: #block has been passed in
+        #     if not len(self.chain): #genesis block
+        #         genesis = existing_block
+        #         genesis.prev_hash = 0
+        #         genesis.hash_block()
+        #         self.chain.append(genesis)
+        #         return genesis.__dict__()
+        #     else: #there are blocks in the block chain
+        #         existing_block.prev_hash = self.chain[-1].hash
+        #         existing_block.index = self.chain[-1].index + 1
+        #         existing_block.hash_block()
+        #         self.chain.append(existing_block)
+        #         return existing_block
+
+       
+    
+    
+
+    # def hash_blocks(self):
+    #     for i,b in enumerate(self.chain):
+    #         if i == 0:
+    #             prev_hash = 0
+    #             genesis = b
+    #             genesis.hash_block(prev_hash)
+    #         else:
+    #             prev_hash = self.chain[i-1].hash
+    #             b.hash_block(prev_hash)
+
+    # def set_difficulty(self,difficulty):
+    #     zeros = ''
+    #     for i in range(difficulty):
+    #         zeros += '0'
+    #     # zeros = zeros[1:]
+    #     for i,b in enumerate(self.chain):
+    #         b.hash = zeros + b.hash
+    #         if i == 0:
+    #             b.prev_hash = 0
+    #             continue
+    #         b.prev_hash = self.chain[i-1].hash
+    #     return self.chain
+        
+    #     return zeros
+    
+    # def check_difficulty(self,difficulty):
+    #     genesis_hash = self.chain[0].hash
+    #     for i,c in enumerate(genesis_hash):
+    #         if c != '0':
+    #             break
+    #     return i >= difficulty
+
+    
+    
+
 
 
 
